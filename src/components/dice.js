0e53b2b6b4d7edc665f17220b4b18b45b5876e74
jmart6784 <jmartinez6784@yahoo.com>
2021-03-18 22:21:46 -0700

    Added Dice images to react state

--- src/components/dice.js
+++ src/components/dice.js
@@ -3,8 +3,12 @@ import React, { useState } from "react";
 const Dice = () => {
   const [playerScore, setPlayerScore] = useState(0);
   const [computerScore, setComputerScore] = useState(0);
+
   const [draws, setDraws] = useState(0);
   const [rounds, setRounds] = useState(0);
+
+  const [playerDice, setPlayerDice] = useState(1);
+  const [computerDice, setComputerDice] = useState(1);
 
   const randomNumber = (myMin, myMax) => {
     return Math.floor(
@@ -13,14 +17,15 @@ const Dice = () => {
   };
 
   const handleSpin = () => {
-    console.log(randomNumber(1, 6));
+    setPlayerDice(randomNumber(1, 6));
+    setComputerDice(randomNumber(1, 6));
 
-    let playerDice = randomNumber(1, 6);
-    let computerDice = randomNumber(1, 6);
+    let pDice = playerDice;
+    let cDice = computerDice;
 
-    if (playerDice > computerDice) {
+    if (pDice > cDice) {
       setPlayerScore(playerScore + 1);
-    } else if (playerDice === computerDice) {
+    } else if (pDice === cDice) {
       setDraws(draws + 1);
     } else {
       setComputerScore(computerScore + 1);
@@ -35,7 +40,12 @@ const Dice = () => {
         <h1 className="p-name">Player</h1>
         <p>Score: {playerScore}</p>
 
-        <img src="images/1.jpg" alt="dice" height="150" width="150" />
+        <img
+          src={`images/${playerDice}.jpg`}
+          alt="dice"
+          height="150"
+          width="150"
+        />
       </div>
 
       <div>
@@ -47,7 +57,12 @@ const Dice = () => {
         <h1 className="p-name">Computer</h1>
         <p>Score: {computerScore}</p>
 
-        <img src="images/1.jpg" alt="dice" height="150" width="150" />
+        <img
+          src={`images/${computerDice}.jpg`}
+          alt="dice"
+          height="150"
+          width="150"
+        />
       </div>
 
       <button id="spin" onClick={() => handleSpin()}>
