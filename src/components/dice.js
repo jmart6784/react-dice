693cd52a183a74e29e8df4b5abb5f52ab08ee112
jmart6784 <jmartinez6784@yahoo.com>
2021-03-18 20:20:58 -0700

    Added state for scores

--- src/components/dice.js
+++ src/components/dice.js
@@ -1,5 +1,17 @@
+import React, { useState } from "react";
+
 const Dice = () => {
-  return <div>DICE</div>;
+  const [playerScore, setPlayerScore] = useState(0);
+  const [computerScore, setComputerScore] = useState(0);
+  const [draws, setDraws] = useState(0);
+
+  return (
+    <div id="container">
+      <div id="player-div"></div>
+
+      <div id="computer-div"></div>
+    </div>
+  );
 };
 
 export default Dice;
