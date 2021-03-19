cdf9750588092c17c48ed1638419cb79600e94c2
jmart6784 <jmartinez6784@yahoo.com>
2021-03-18 21:54:33 -0700

    Added random number function

--- src/components/dice.js
+++ src/components/dice.js
@@ -6,20 +6,34 @@ const Dice = () => {
   const [draws, setDraws] = useState(0);
   const [rounds, setRounds] = useState(0);
 
+  const randomNumber = (myMin, myMax) => {
+    return Math.floor(
+      Math.random() * (Math.ceil(myMax) - Math.floor(myMin) + 1) + myMin
+    );
+  };
+
+  const handleSpin = () => {
+    console.log(randomNumber(1, 6));
+  };
+
   return (
     <div id="container">
       <div id="player-div">
         <h1 className="p-name">Player</h1>
+        <p>Score: {playerScore}</p>
 
-        <img src="images/1.jpg" alt="dice-1" height="150" width="150" />
+        <img src="images/1.jpg" alt="dice" height="150" width="150" />
       </div>
 
-      <button id="spin">Spin</button>
+      <button id="spin" onClick={() => handleSpin()}>
+        Spin
+      </button>
 
       <div id="computer-div">
         <h1 className="p-name">Computer</h1>
+        <p>Score: {computerScore}</p>
 
-        <img src="images/1.jpg" alt="dice-1" height="150" width="150" />
+        <img src="images/1.jpg" alt="dice" height="150" width="150" />
       </div>
     </div>
   );
