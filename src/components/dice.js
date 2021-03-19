851b60a061bdf7e64d10a6b1b5da9fe0895eea02
jmart6784 <jmartinez6784@yahoo.com>
2021-03-18 21:05:18 -0700

    Added dice images

--- src/components/dice.js
+++ src/components/dice.js
@@ -4,12 +4,21 @@ const Dice = () => {
   const [playerScore, setPlayerScore] = useState(0);
   const [computerScore, setComputerScore] = useState(0);
   const [draws, setDraws] = useState(0);
+  const [rounds, setRounds] = useState(0);
 
   return (
     <div id="container">
-      <div id="player-div"></div>
+      <div id="player-div">
+        <h1 className="p-name">Player</h1>
 
-      <div id="computer-div"></div>
+        <img src="images/1.jpg" alt="dice-1" height="150" width="150" />
+      </div>
+
+      <button id="spin">Spin</button>
+
+      <div id="computer-div">
+        <h1 className="p-name">Computer</h1>
+      </div>
     </div>
   );
 };
